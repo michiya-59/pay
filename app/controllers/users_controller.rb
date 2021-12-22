afdd62726964e4844d567852ed0e17e40390c5d4
Nishino <m.n.tennis.0509@icloud.com>
2021-12-22 22:54:27 +0900

    --auto-correctの一律修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,10 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
-      redirect_to users_url
-    end
+    redirect_to users_url if user.save
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
