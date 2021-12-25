a289445dfbf1509f97205d1e48d95a160e5f20ac
西野 倫也 <nishino@zeku.co.jp>
2021-12-25 11:26:41 +0900

    本業収入名を入れるテーブル等を作った

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user
+      sessions[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to login_url
+    end
+  end
 end
