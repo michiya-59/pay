c6da3323bf18361a62446109525b63df219f8f64
西野 倫也 <nishino@zeku.co.jp>
2022-01-18 13:54:07 +0900

    各月の収入の作成の実装完了

--- app/helpers/incomes_helper.rb
+++ app/helpers/incomes_helper.rb
@@ -14,7 +14,7 @@ module IncomesHelper
   end
 
   def month_array
-    month_array = [1,2,3,4,5,6,7,8,9,0,11,12]
+    month_array = [1,2,3,4,5,6,7,8,9,10,11,12]
   end
 
   # ハッシュ値からvaluesの値段を一つずつ取り出し,合計を求めているメソッド
