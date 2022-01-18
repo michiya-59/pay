24824c11b76fd95d327beb67553f63426c9b4fb8
西野 倫也 <nishino@zeku.co.jp>
2022-01-18 11:57:43 +0900

    importmap_tagでjsを使えるようにコード修正した

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,1 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-import "@hotwired/turbo-rails"
-import "controllers"
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import '@fortawesome/fontawesome-free/js/all'
-
-//= require jquery
-//= require jquery_ujs
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
-
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "application", preload: true
+pin "name", to: "https://ga.jspm.io/npm:name@0.0.2/core.js"
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/index.js"
+pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.20.2/index.js"
+pin "react", to: "vendor/react@17.0.2.js"
